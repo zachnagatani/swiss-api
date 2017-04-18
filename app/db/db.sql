6081cd4cfc23f31c1b749125ab856338eedec8d8
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-18 15:03:44 -0700

    Fix: add explicit foreign keys

--- app/db/db.sql
+++ app/db/db.sql
@@ -1,13 +1,17 @@
 CREATE TABLE players (
-    id INT(7) AUTO_INCREMENT PRIMARY KEY,
+    id TINYINT UNSIGNED AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     wins INT,
-    losses INT
+    losses INT,
+    PRIMARY KEY(id)
 );
 
 CREATE TABLE matches (
-    id INT(7) AUTO_INCREMENT PRIMARY KEY,
+    id TINYINT UNSIGNED AUTO_INCREMENT,
     round INT NOT NULL,
     winner INT NOT NULL,
-    loser INT NOT NULL
+    loser INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (winner) REFERENCES players(id),
+    FOREIGN KEY (loser) REFERENCES players(id)
 );
