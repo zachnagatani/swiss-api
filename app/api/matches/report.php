2cce52c3f5ac0b71f1882e713f6cc9ab4c486287
Zachariah Nagatani <zachnagatani@gmail.com>
2017-04-18 14:46:28 -0700

    Chore: Refactor statement executions for reporting matches

--- app/api/matches/report.php
+++ app/api/matches/report.php
@@ -6,22 +6,52 @@
         try {
             $db = Db::connect();
 
+            // Variables from POST values
+            $round = $request->getParam('round');
+            $winner = $request->getParam('winner');
+            $loser = $request->getParam('loser');
+
+            // Array of sql statements, empty initially
+            $stmts = array();
             // Prepare
             $sql = "INSERT INTO matches (round, winner, loser)
                     VALUES (:round, :winner, :loser)";
             $stmt = $db->prepare($sql);
+            array_push($stmts, $stmt);
 
             // Bind
             $stmt->bindParam(':round', $round);
             $stmt->bindParam(':winner', $winner);
             $stmt->bindParam(':loser', $loser);
 
-            $round = $request->getParam('round');
-            $winner = $request->getParam('winner');
-            $loser = $request->getParam('loser');
+            // Update players' records
 
-            // Execute
-            $stmt->execute();
+            // Winner
+            // Prepare
+            $sql = "UPDATE players
+                    SET wins = wins + 1
+                    WHERE id = :winner";
+            $stmt = $db->prepare($sql);
+            array_push($stmts, $stmt);
+
+            // Bind
+            $stmt->bindParam(':winner', $winner);
+
+            // Loser
+            // Prepare
+            $sql = "UPDATE players
+                    SET losses = losses + 1
+                    WHERE id = :loser";
+            $stmt = $db->prepare($sql);
+
+            // Bind
+            $stmt->bindParam(':loser', $loser);
+            array_push($stmts, $stmt);
+
+            // Loop through $stmts and execute each one
+            foreach($stmts as $stmt) {
+                $stmt->execute();
+            }
 
             $data = array(
                 "Success" => True,
